9499aaf75bb79cfc9cf838e1009c84666402ef02
Franco Gatti <francogatti.sdp@outlook.com>
2025-04-08 20:35:43 -0300

    Detalles

--- src/routes/mocks.router.js
+++ src/routes/mocks.router.js
@@ -5,12 +5,17 @@ import usersModel from "../models/users.model.js";
 const mocksRouter = Router()
 
 mocksRouter.get('/mockingusers', (req, res) => {
-    const { amount = 50 } = req.query
-    const users = []
-    for (let i = 0; i < amount; i++) {
-        users.push(createUser())
+    try {
+        const { amount = 50 } = req.query
+        const users = []
+        for (let i = 0; i < amount; i++) {
+            users.push(createUser())
+        }
+        res.status(200).send(users)
     }
-    res.send(users)
+    catch (e) {
+        res.status(500).send(e)
+    }
 })
 
 mocksRouter.post('/generatedata', async (req, res) => {
@@ -19,14 +24,14 @@ mocksRouter.post('/generatedata', async (req, res) => {
         const users = []
 
         for (let i = 0; i < amount; i++) {
-            const user =createUser()
+            const user = createUser()
             users.push({
                 ...user,
-                _id:user.id
+                _id: user.id
             })
         }
         await usersModel.insertMany(users)
-        res.status(200).send("Usuarios agregados con Éxito")
+        res.status(201).send("Usuarios agregados con Éxito")
     } catch (e) {
         res.status(400).send(e)
     }
